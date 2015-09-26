369ab0cf156be6fc71a4692fd7acfb61bdae4429
Askar Akhmerov <askar.akhmerov@smava.de>
2015-09-26 18:14:41 +0200

    add basic room stub

--- apps/de.tvhack15.app.TVKids/Contents/Javascript/Views/MainView.js
+++ apps/de.tvhack15.app.TVKids/Contents/Javascript/Views/MainView.js
@@ -1,9 +1,13 @@
 var MainView = new MAF.Class({
     ClassName: 'MainView',
+    RoomName : 'Theater-Room',
 
 	Extends: MAF.system.FullscreenView,
 
-	initialize: function () { this.parent(); },
+	initialize: function () {
+        this.parent();
+        this.room = new MAF.PrivateRoom(this.RoomName);
+    },
 
 	createView: function () {
         var view = this;
@@ -51,24 +55,81 @@ var MainView = new MAF.Class({
                             wrap: true
                         }
                     }).appendTo(cell);
+                    var connectionButton = new MAF.element.Image({
+                        src : 'Images/Button_Connect.jpg',
+                        styles: {
+                            color: 'white',
+                            fontSize: 30,
+                            anchorStyle: 'center',
+                            wrap: true,
+                            vOffset:backButton.height + 10
+                        }
+                    }).appendTo(cell);
                 }
             }
         }).appendTo(view);
-//        var background = new MAF.element.Image({
-//
-//            styles: {
-//                width: view.width,
-//                height: view.height,
-//                fontSize: 60,
-//                anchorStyle: 'center',
-//                backgroundRepeat: 'none',
-//                backgroundSize: '100%'
-//                }
-//            }).appendTo(view);
 
         view.elements.elementGrid.changeDataset([
             {},{},{},{}
-        ], true)
+        ], true);
+
+        this.initRoom(view);
+
+    },
+
+    initRoom : function (view) {
+        // Set listeners for Room and Connection
+        var room = view.room;
+        (function (event) {
+            var payload = event.payload;
+            switch (event.type) {
+                case 'onConnected':
+                    log('room connected');
+                    // If connected but room not joined make sure to join it automaticly
+                    if (!room.joined) room.join();
+                    return;
+                case 'onDisconnected':
+                    clients = {}; // Reset clients
+                    log('connection lost waiting for reconnect and automaticly rejoin');
+                    return;
+                case 'onCreated':
+                    // Create an url to the client application and pass the hash as querystring
+                    var url = widget.getUrl('Client/theater.html?hash=' + payload.hash);
+                    log('room created', payload.hash, url);
+                    return;
+                case 'onDestroyed':
+                    clients = {}; // Reset clients
+                    log('room destroyed', payload.hash);
+                    return;
+                case 'onJoined':
+                    // If user is not the app then log the user
+                    if (payload.user !== room.user)
+                        log('user joined', payload.user);
+                    return;
+                case 'onHasLeft':
+                    // If user is not the app then log the user
+                    if (payload.user !== room.user)
+                        log('user has left', payload.user);
+                    return;
+                case 'onData':
+                    var data = payload.data;
+                    if (data.e === 'draw')
+                        return;
+                    if (data.e === 'clear')
+                        return this.reset();
+                    break;
+                default:
+                    log(event.type, payload);
+                    break;
+            }
+        }).subscribeTo(view.room,
+            ['onConnected', 'onDisconnected', 'onCreated', 'onDestroyed', 'onJoined', 'onHasLeft', 'onData', 'onError']);
+        room.join();
+    },
+
+    // Reset the Canvas
+    reset : function() {
+
     },
 
 	updateView: function () {
--- apps/de.tvhack15.app.TVKids/Contents/Javascript/init.js
+++ apps/de.tvhack15.app.TVKids/Contents/Javascript/init.js
@@ -1,8 +1,10 @@
 include("Javascript/Views/MainView.js");
+include('Javascript/Views/Room.js');
 
 MAF.application.init({
     views: [
         { id: 'view-MainView', viewClass: MainView },
+        { id: 'view-Room', viewClass: Room },
         { id: 'view-AboutView', viewClass: MAF.views.AboutBox }
     ],
     defaultViewId: 'view-MainView',
